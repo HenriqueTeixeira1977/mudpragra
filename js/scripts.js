8642d3c99cd79f0a1cf19b33b583f21b078e517c
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-05-08 16:59:22 -0300

    UpDate080520231659

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 //  <!-- Google tag (gtag.js) -->
-//  <script async src="https://www.googletagmanager.com/gtag/js?id=G-C9T5LX00SX"> </script>
+
+//  <script async src="https://www.googletagmanager.com/gtag/js?id=G-C9T5LX00SX"></script>
+
 //  <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
