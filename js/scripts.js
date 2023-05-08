70dae5eaae3e628b5fceab3545e722374bf70485
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2023-05-08 20:38:51 -0300

    UpDate080520232038

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,25 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-PCWRWW3');
 //  </script>
 //  <!-- End Google Tag Manager -->
+
+
+//  Script para navegação bar com efeito de on scroll - sticky 
+
+window.addEventListener("scroll", function(){
+	var header = document.querySelector("header");
+	header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+//Script para Navigation sidebar menu responsivo
+
+var menu = document.querySelector('.menu');
+var menuBtn = document.querySelector('.menu-btn');
+var closeBtn = document.querySelector('.close-btn');
+
+menuBtn.addEventListener("click", () => {
+	menu.classList.add('active');
+});
+
+closeBtn.addEventListener("click", () => {
+	menu.classList.remove('active');
+});
