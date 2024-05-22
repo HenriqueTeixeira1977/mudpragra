4c590373e99c858c8ac7dd5ed8ff3823cafdb158
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2024-05-22 17:26:18 -0300

    Update20240522173658

--- js/scripts.js
+++ js/scripts.js
@@ -22,11 +22,11 @@ closeBtn.addEventListener("click", () => {
 //  <!-- Google tag (gtag.js) -->
 //  <script async src="https://www.googletagmanager.com/gtag/js?id=G-C9T5LX00SX"></script>
 //  <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'G-C9T5LX00SX');
+gtag('config', 'G-C9T5LX00SX');
 //  </script>
 
 
